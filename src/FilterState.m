39c4e2c5d0eb6458804e771267b6ebc2d208f3d8
Connor Brashar <Conrolder@Brawne-Lamia.local>
2018-11-05 21:17:43 -0600

    Added Sensor Measurements

--- src/FilterState.m
+++ src/FilterState.m
@@ -1,4 +1,4 @@
-function [FS] = FilterState(S, M, P, t)
+function [FS] = FilterState(S, M, P, C, t)
 %% FilterState
 % Author: 
 %   Tucker Haydon, Connor Brashar
@@ -8,11 +8,36 @@ function [FS] = FilterState(S, M, P, t)
 %   S - Struct containing previous state estimate
 %   M - Struct containing sensor measurements at current time
 %   P - Struct containing filter parameters at current time
-%   t - scalar representing current time
+%   U - Struct containing control input parameters at current time
+%   t - scalar representing current time. Updates at 100Hz.
 % Return Values:
 %   ES - Struct containing current state estimate
+%% States and Matrices
+A = [zeros(3), eye(3); zeros(3,6)];
+
+B = [zeros(3,6); zeros(3), eye(3)];
+
+Cul = [1,0,0; 1,-1,0; 0,1,-1];
+Cm = [Cul, zeros(3); zeros(3,6)];
+
+D = B;
+
+X = [S.x; S.v];
+U = [zeros(3,1); C.a];
+
+% Z has a varied length depending on what timestep we are on
+if exist(M.x)
+    Z = [M.x; M.dx; M.a];
+elseif exist(M.dx)
+    Z = [M.dx; M.a];
+else
+    Z = M.a;
+end
 
 %% Function
 
 
-end
+ 
+ 
+ 
+ end
--- src/GenerateSensorMeasurements.m
+++ src/GenerateSensorMeasurements.m
@@ -1,4 +1,4 @@
-function [M] = GenerateSensorMeasurements(S, P, t)
+function [M] = GenerateSensorMeasurements(S, P, C, t)
 %% GenerateSensorMeasurements
 % Author: 
 %   Tucker Haydon, Connor Brashar
@@ -7,11 +7,38 @@ function [M] = GenerateSensorMeasurements(S, P, t)
 % Parameters:
 %   S - Struct containing true system state at current time
 %   P - Struct containing sensor parameters at current time
+%   C - Struct containing the zero-order hold on the last control input
 %   t - scalar representing current time
 % Return Values:
 %   M - Struct containing system measurements at current time
+% Notes:
+%   1. S.x, S.v are to be the system components, where S.x has three
+%      parts, and so does S.v. (one for each vehicle)
+%   2. C.a is the acceleration control input on the vehicles
+%   3. P.IMU, P.range, and P.GPS denote the standard deviations for each
+%      component
+%   4. M is the measurement matrix (also known as Z), and has M.a 
+%      (3x1 accel), M.x1 (1x1 GPS measurement), and M.dx (2x1 range 
+%      measurements). Measurements are only generated when a state is
+%      needed.
 
 %% Function
 
+M.a = C.a + 0.5 .* randn(3,1); % 0.5 being the error every single sample
+
+% Create range measurements
+if mod(t,0.1) == 0
+    
+    M.dx = [S.x(1) - S.x(2); S.x(2) - S.x(3)];
+    M.dx = M.dx + 10 .* randn(2,1);
+    
+end
+
+% Create GPS measurements
+if mod(t,1) == 0
+    
+    M.x = S.x(1) + 3.0 .* randn(1);
+
+end
 
 end
